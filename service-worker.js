ca26cf7af20c49125e16c26ff9618f547b7b52cd
Bharat.io <theadeebalam@gmail.com>
2026-03-10 18:53:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rwa-parking-cache-v1";
+const CACHE_NAME = "rwa-parking-cache-v2";
 
 const urlsToCache = [
   "./",
@@ -9,6 +9,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -17,30 +18,33 @@ self.addEventListener("install", event => {
   );
 
   self.skipWaiting();
+
 });
 
 self.addEventListener("activate", event => {
-
-  const cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
+
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
+
         })
       );
     })
   );
 
   self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
 
   event.respondWith(
+
     caches.match(event.request)
       .then(response => {
 
@@ -51,6 +55,7 @@ self.addEventListener("fetch", event => {
         return fetch(event.request);
 
       })
+
   );
 
 });
